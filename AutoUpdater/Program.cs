22db86c28c3f4309e86422c0cc659a8574575256
SW_Creeperking <sw.creeperking@gmail.com>
2023-03-31 17:16:54 -0700

    Initial commit

---
+++ AutoUpdater/Program.cs
@@ -0,0 +1,149 @@
+﻿using System.IO.Compression;
+
+public class Program
+{
+    public static readonly string CurrentDirectory =
+        Environment.ProcessPath.Remove(Environment.ProcessPath.Replace("\\", "/").LastIndexOf('/'));
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine($"STARTING UPDATER WITH ARGS: [{string.Join(",", args)}]\n\n");
+        Console.WriteLine($"current path: [{CurrentDirectory}]");
+        try
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Arguments are incorrect");
+                Continue();
+                return;
+            }
+
+            var downloadSite = args[0];
+            var downloadFolder = args[1];
+            var cleanPath = args.Length > 2 && bool.Parse(args[2]);
+            var zipType = args.Length > 3 ? args[3] : "none";
+            var closeOnComplete = args.Length > 4 && bool.Parse(args[4]);
+            var copySource = args.Length > 5 ? args[5] : null;
+
+            if (cleanPath)
+            {
+                Console.WriteLine($"Cleaning path: [{downloadFolder}]");
+                CleanPath(downloadFolder);
+            }
+
+            var fileName = downloadSite.Split('/')[^1];
+            Console.WriteLine($"Downloading: [{fileName}]");
+            Download(downloadSite, fileName).GetAwaiter().GetResult();
+            Console.WriteLine($"Downloaded: [{fileName}]");
+
+            Console.WriteLine("Unzipping");
+            UnZip(zipType, fileName, downloadFolder);
+            Console.WriteLine("Unzipped");
+
+            if (copySource is not null)
+            {
+                MovePath(copySource, downloadFolder);
+                Directory.Delete(copySource);
+            }
+
+            if (closeOnComplete) return;
+            Continue();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Exception Found!");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(e);
+            Console.ResetColor();
+            Continue();
+        }
+    }
+
+    private static async Task Download(string url, string fileName)
+    {
+        using var client = new HttpClient();
+        await using var clientStream = await client.GetStreamAsync(url);
+        await using var fileStream = new FileStream(fileName, FileMode.CreateNew);
+        await clientStream.CopyToAsync(fileStream);
+    }
+
+    private static bool CleanPath(string path)
+    {
+        if (path == CurrentDirectory) return false;
+
+        foreach (var file in Directory.GetFiles(path))
+        {
+            Console.WriteLine($"deleting [{file}]");
+
+            while (true)
+            {
+                try
+                {
+                    File.Delete(file);
+                    break;
+                }
+                catch (AccessViolationException)
+                {
+                    Console.WriteLine($"file: [{file}] is in use, waiting 2s to try again . . .");
+                    Task.Delay(2000).GetAwaiter().GetResult();
+                }
+            }
+        }
+
+        foreach (var folder in Directory.GetDirectories(path))
+        {
+            if (!CleanPath(folder)) continue;
+
+            Console.WriteLine($"deleting [{folder}]");
+
+            while (true)
+            {
+                try
+                {
+                    Directory.Delete(folder);
+                    break;
+                }
+                catch (AccessViolationException)
+                {
+                    Console.WriteLine($"folder: [{folder}] is in use, waiting 2s to try again . . .");
+                    Task.Delay(2000).GetAwaiter().GetResult();
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private static void MovePath(string path, string dest)
+    {
+        foreach (var file in Directory.GetFiles(path))
+        {
+            File.Move(file,
+                $"{dest}{file[file.Replace("\\", "/").LastIndexOf('/')..]}");
+        }
+
+        foreach (var dir in Directory.GetDirectories(path))
+        {
+            Directory.Move(dir,
+                $"{dest}{dir[dir.Replace("\\", "/").LastIndexOf('/')..]}");
+        }
+    }
+
+    private static void UnZip(string zipType, string file, string dest)
+    {
+        switch (zipType)
+        {
+            case "zip":
+                ZipFile.ExtractToDirectory(file, dest);
+                File.Delete(file);
+                break;
+        }
+    }
+
+    private static void Continue()
+    {
+        Console.WriteLine("Downloader Finished");
+        Console.WriteLine("Press Enter to continue . . .");
+        Console.ReadLine();
+    }
+}
---
+++ README.md
@@ -0,0 +1,37 @@
+This is a downloader, its purpose is to be ran with arguments to download
+
+this is made for <a href="https://github.com/SWCreeperKing/RayWork/tree/master/RayWork.SelfUpdater">
+RayWork.SelfUpdater</a>
+
+### methodology
+
+---
+
+1. main program downloads the downloader in a sub folder
+2. main program runs downloader with arguments
+3. main program exits after running downloader
+4. downloader will clear original directory [optional]
+5. downloader will download from the internet
+6. downloader will extract from zip [optional]
+    - more extractions to support in future (help needed)
+7. downloader will move from extracted folders to original [optional/input needed]
+8. downloader will run main program
+9. main program will delete downloader
+
+## Running Arguments
+
+---
+
+1. download site
+   - site that the download comes from, must be a direct download
+2. download folder
+   - can not be empty directory
+   - has to be the directory to download into
+3. clean path [false]
+   - if to clean/delete everything in download folder
+4. zip type [none]
+   - what compression type is used, currently only `zip` is supported
+5. close on complete [false]
+   - close the downloader console when the download is complete
+6. copy source [null]
+   - if set then it will copy from a directory inside the unzipped folder into the download folder
